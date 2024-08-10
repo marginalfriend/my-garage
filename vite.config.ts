4f7077ca74a7fa9c0e4bc0ea6cc1fdb66a3615bf
marginalfriend <abdullahpsg@gmail.com>
2024-08-10 11:33:52 +0700

    [NEW] create product page

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+	server: {
+		proxy: {
+			'/auth': 'http://localhost:3000',
+			'/categories': 'http://localhost:3000',
+			'/products': 'http://localhost:3000',
+		}
+	}
 })
