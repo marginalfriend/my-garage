43a249ee42910ea03ffb519a4d95f702fc23e1b0
marginalfriend <abdullahpsg@gmail.com>
2025-02-24 02:48:02 +0700

    feat: absolute path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ export default {
 		{
 			name: 'gk5-garage-backend',
 			cwd: '/root/my-garage',
-			script: 'server.js',
+			script: '/root/my-garage/server.js',
 			env: {
 				NODE_ENV: 'development',
 				PORT: 3000
@@ -16,7 +16,7 @@ export default {
 		{
 			name: 'gk5-garage-frontend',
 			cwd: '/root/my-garage',
-			script: 'node_modules/.bin/serve',
+			script: '/root/my-garage/node_modules/.bin/serve',
 			args: 'dist -l 5173',
 			env: {
 				NODE_ENV: 'development'
