f72e473ce781a1525a5ec49ab8963df55d43be97
marginalfriend <abdullahpsg@gmail.com>
2025-02-24 02:41:03 +0700

    feat: idk

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ export default {
 		{
 			name: 'gk5-garage-backend',
 			cwd: '/root/my-garage',
-			script: '/usr/bin/node',
+			script: '/root/.nvm/versions/node/v22.14.0/bin/node',
 			args: 'server.js',
 			env: {
 				NODE_ENV: 'development',
@@ -17,7 +17,7 @@ export default {
 		{
 			name: 'gk5-garage-frontend',
 			cwd: '/root/my-garage',
-			script: '/usr/bin/npx',
+			script: '/root/.nvm/versions/node/v22.14.0/bin/npx',
 			args: 'serve dist -l 5173',
 			env: {
 				NODE_ENV: 'development'
