fbb71d0720f97c4f78aa2cb24edc995cfe80ffb9
marginalfriend <abdullahpsg@gmail.com>
2025-03-12 02:41:18 +0700

    feat: filtering

--- prisma/seed.js
+++ prisma/seed.js
@@ -43,10 +43,10 @@ async function main() {
 	// Assign role to account if it doesn't exist
 	await prisma.accountRole.upsert({
 		where: {
-			accountId_roleId: {
-				accountId: superAdminAccount.id,
-				roleId: superAdminRole.id
-			}
+			AND: [
+				{ accountId: superAdminAccount.id },
+				{ roleId: superAdminRole.id }
+			]
 		},
 		update: {},
 		create: {
@@ -57,10 +57,10 @@ async function main() {
 
 	await prisma.accountRole.upsert({
 		where: {
-			accountId_roleId: {
-				accountId: ownerAccount.id,
-				roleId: ownerRole.id
-			}
+			AND: [
+				{ accountId: superAdminAccount.id },
+				{ roleId: superAdminRole.id }
+			]
 		},
 		update: {},
 		create: {
